22c2c8be95fbf1e74d9726b3eb9ff30bca165148
lscheinhorn <lukescheinhorn@gmail.com>
2022-01-19 20:23:59 -0500

    feat: like button improve, search date (not finished), getNasaImages()

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ const info = {
     ACCOUNT_ID: "cb241c79-b1fd-45dd-8067-36bdea054751" 
 }
 
+//stores api parameters and builds request URL
+let count = 3;
+let date;
+let requestURL = () => {
+    let url = `https://api.nasa.gov/planetary/apod?api_key=${info.API_KEY}`;
+    if(count) {
+        url += `&count=${count}`;
+    }
+    if(date) {
+        url += `&date=${date}`;
+    }
+    return url;
+}
+
 //Changes like status
 const likeToggle = (event) => {
     console.log("Yay! You like me!")
@@ -15,52 +29,70 @@ const likeToggle = (event) => {
         event.target.ariaLabel = "Like";                }
 }
 
-//http request for image data from apoc api
-fetch(`https://api.nasa.gov/planetary/apod?api_key=${info.API_KEY}&count=3`)
-    .then((response) => {
-      
-        //parsing JSON file to javascript object
-        return response.json()
-     })
-    .then((data) => {
-        console.log(data);
-       
-        //iterating through image objects to capture data
-        data.forEach((imageObj) => {
-           
-            //create a div for image
-            let newDiv = document.createElement("div");
 
-            //create and append the image to parent div
-            let img = document.createElement("img");
-            img.src = imageObj.url;
-            newDiv.appendChild(img);
-            
-            //creating a like button
-            let likeButton = document.createElement("button");
-            likeButton.addEventListener("click", likeToggle);
-            likeButton.innerText = "Like";
-            likeButton.ariaLabel = "Like";
-            newDiv.appendChild(likeButton);
-            
-            //create and append the title to parent div
-            let title = document.createElement("h4");
-            title.innerText = imageObj.title;
-            newDiv.appendChild(title);
-            
-            //create and append the date to parent div
-            let date = document.createElement("p");
-            date.innerText = `Posted: ${imageObj.date}`;
-            newDiv.appendChild(date);
-            
-            //create and append the description to parent div
-            let description = document.createElement("p");
-            description.innerText = imageObj.explanation;
-            newDiv.appendChild(description);
-            
-            //append the new div to the 'main' section
-            document.getElementById("main").appendChild(newDiv);
+
+//makes http request for image data from apoc api
+const getNasaImages = () => {
+    fetch(requestURL())
+        .then((response) => {
+        
+            //parsing JSON file to javascript object
+            return response.json()
         })
-    })
-    
+        .then((data) => {
+            console.log(data, "data object");
+        
+            //puts object with one image in an array to perform .forEach()
+            if(!Array.isArray(data)) {
+                data = [data];
+            }
+        
+            //iterating through image objects to capture data
+            data.forEach((imageObj) => {
+                console.log(imageObj)
+                //create a div for image
+                let newDiv = document.createElement("div");
 
+                //create and append the image to parent div
+                let img = document.createElement("img");
+                img.src = imageObj.url;
+                newDiv.appendChild(img);
+                
+                //creating a like button
+                let likeButton = document.createElement("button");
+                likeButton.addEventListener("click", likeToggle);
+                likeButton.innerText = "Like";
+                likeButton.ariaLabel = "Like";
+                newDiv.appendChild(likeButton);
+                
+                //create and append the title to parent div
+                let title = document.createElement("h4");
+                title.innerText = imageObj.title;
+                newDiv.appendChild(title);
+                
+                //create and append the date to parent div
+                let date = document.createElement("p");
+                date.innerText = `Posted: ${imageObj.date}`;
+                newDiv.appendChild(date);
+                
+                //create and append the description to parent div
+                let description = document.createElement("p");
+                description.innerText = imageObj.explanation;
+                newDiv.appendChild(description);
+                
+                //append the new div to the 'main' section
+                document.getElementById("main").appendChild(newDiv);
+            })
+            
+        })
+    }
+
+getNasaImages();
+    
+const searchDate = () => {
+    console.log('search date function');
+    //date = "document.getElementById("date_input").value";
+    //count = 0;
+    document.getElementsByTagName("section")[0].remove;
+    //getNasaImages();
+}
